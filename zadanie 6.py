88c774fc1f1f8017867584b66e612298ad735d99
ystepanov-beep <y.stepanov@g.nsu.ru>
2026-03-27 13:14:50 +0700

    Update zadanie 6.py

--- zadanie 6.py
+++ zadanie 6.py
@@ -2,7 +2,8 @@ sms = input("")
 
 
 def check_for_limit(sms: str) -> str:
-    """Returns the message if it is within the 160-character limit, otherwise returns the first 160 characters."""
+    """Returns the message if it is within the 160-character limit, 
+    otherwise returns the first 160 characters."""
 
     if len(sms) <= 160:
         return sms
